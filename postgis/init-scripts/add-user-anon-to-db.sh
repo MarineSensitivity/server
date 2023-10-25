5fac9f2fc6bcb33e2ac22b410ef2de2139902880
Ben Best <ben@ecoquants.com>
2023-10-25 14:51:42 -0400

    pg fixes

--- postgis/init-scripts/add-user-anon-to-db.sh
+++ postgis/init-scripts/add-user-anon-to-db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "admin" --dbname "$PASSWORD" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username "admin" --dbname "msens" <<-EOSQL
     CREATE USER anon WITH PASSWORD '$ANONPASSWORD';
     GRANT CONNECT ON DATABASE msens TO anon;
     GRANT USAGE ON SCHEMA public TO anon;
